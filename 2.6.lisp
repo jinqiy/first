cf2c98bd883c694d7a8d55b9454f4280e32ab8ad
laoyin <jinqi.yin@gmail.com>
2014-01-28 17:11:35 +0800

    2.6.lisp

---
+++ 2.6.lisp
@@ -0,0 +1,14 @@
+a)
+[1]> (car (car  (cdr '(a (b c)d))))                                                                                     
+B
+
+b)
+(+ 13 (/ 1 0 ))
+
+*** - /: division by zero
+The following restarts are available:
+ABORT          :R1      Abort main loop
+
+c)
+[23]> (apply #'list 1 nil)
+(1)
