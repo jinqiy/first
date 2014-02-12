a9d657ba4dea3c613068d242998356a8c958f701
laoyin <jinqi.yin@gmail.com>
2014-02-12 17:03:22 +0800

    2.9.lisp

---
+++ 2.9.lisp
@@ -0,0 +1,23 @@
+(a)
+should assign lst without nil to a new variable before add up
+[26]> 
+(defun summit (lst)
+  (setf lstx (remove nil lst))
+  (apply #'+ lstx))
+SUMMIT
+[27]> (summit '(1 2 3 nil))
+6
+
+(b)
+should have the teminal statment
+[43]>
+(defun summit (lst)
+  (let ((x (car lst)))
+    (if (null (cdr lst)) 
+      0 
+      (if (null x)  
+        (summit (cdr lst))
+	    (+ x (summit (cdr lst)))))))
+SUMMIT
+[44]> (summit '(1 2 3 nil))
+6
