74b1e6a81fe5ccb21f65e0c0391ee49c9295dcdf
laoyin <jinqi.yin@gmail.com>
2014-03-20 16:41:50 +0800

    3.6.lisp modified

--- 3.6.lisp
+++ 3.6.lisp
@@ -1,16 +1,49 @@
 (a) cons  but cons is a primitive function, nothing changed here.
+OR, I can try make it a little different. 
 [12]>
-(defun mcons (a b)
-(cons a b)
-)
-MCONS
-[13]> (mcons 'a '(b c))                                                                                            
-(A B C)
-[14]> (mcons '(a a) '(b c))                                                                                        
-((A A) B C)
+(defun mycons (x y)
+`(,x . ,y))
+MYCONS
+[95]> (cons 'a 'b)
+(A . B)
+[101]> (mycons '(a b) 'c)
+((A B) . C)
+[102]> (mycons '(a b) '(c d))
+((A B) C D)
 
 (b) list   also, list is a primitive function, nothing changed here.
+OR, a tricky.
+[112]> 
+(defun mylist (&rest x)
+  `(,@x)
+)
+MYLIST
+[113]> (mylist 'a)
+(A)
+[114]> (mylist 'a 'b)
+(A B)
+[115]> (mylist 'a 'b 'c)
+(A B C)
+[116]> (mylist 'a '(b c))
+(A (B C))
 
 (c) length(for lists)
+kindly changed cdr to car
+[1]> 
+(defun mlength (lst)
+(if (null lst)
+  0
+  (+ 1 (mlength (car lst)))
+)
+)
+MLENGTH
+[2]> (mlength '(1))
+1
+[3]> (mlength '(1 2 3))
+3
+[4]> (mlength nil)
+0
+[5]> (mlength '())
+0
 
 
